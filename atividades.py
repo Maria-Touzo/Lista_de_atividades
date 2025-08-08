feabc7cc369d9bf8b393ec5c8a6537a27af58de7
Maria Julia <maria.touzo@aluno.senai.br>
2025-08-08 10:49:09 -0300

    finalmente finalizei!

--- atividades.py
+++ atividades.py
@@ -1,4 +1,4 @@
- #criando a lista de afazeres
+#criando a lista de afazeres
 atividades = []
 
 while True:
@@ -16,23 +16,39 @@ while True:
 
 
     escolha = input("Qual opção da lista acima você deseja? ")
-    if escolha == "0":
-        print("Certo, você saiu da sua lista de afazeres.")  
+
     if escolha == "1": #se a opção escolhida pela pessoa for = 1, irá inserir tarefa
         print("Você escolheu a opção de inserir tarefas.")   
         tarefa_acrescentada = input("Qual tarefa você deseja acrescentar na lista? ")
         atividades.append(tarefa_acrescentada) #acrescentando tarefas na lista
-    if escolha == "2":#se a opção escolhida pela pessoa for = 2, irá listar as tarefas
-        print("Aqui está a sua lista de  tarefas: ")  
-        print(atividades) #imprimindo a lista 
-    if escolha == "3": #se a opção escolhida pela pessoa for = 3, a tarfa será marcada como concluida 
-        print("Você escolheu a opção de marcar tarefa como concluida")  
-        if escolha == "concluida":
-            print ("tarefa concluida")
 
+    if escolha == "2":#se a opção escolhida pela pessoa for = 2, irá listar as tarefas
+        cont = 0 
+        for tarefa in atividades:
+            print(f"{cont} - {tarefa}")
+            cont += 1
+
+    if escolha == "3": #se a opção escolhida pela pessoa for = 3, a tarfa será marcada como concluida 
+        cont = 0 
+        for tarefa in atividades:
+            print(f"{cont} - {tarefa}")
+            cont += 1
+        tarefa_concluida =  int(input("Qual tarefa você deseja concluir? (escolha por número) "))
+        atividades[tarefa_concluida] = atividades[tarefa_concluida] + "(X)"
+       
     if escolha == "4": #se a opção escolhida pela pessoa for = 4, a tarefa será removida da lista
         print("Você escolheu a opção de remover tarefa")
-        item_remover = input("Qual ítem você deseja remover? ")
-        atividades.remove(item_remover) #removendo ítem da lista
+        item_remover = int(input("Qual ítem você deseja remover? (escolha plo numero da tarefa) "))
+        atividades.pop(item_remover) #removendo ítem da lista
+
+    if escolha == "0":
+        print("Certo, você saiu da sua lista de afazeres.")  
+        with  open ("banco_de_dados.txt", "w") as arquivo: #abrindo o arquivo em txt
+            for tarefa in atividades:
+                arquivo.write(tarefa + "\n") #escrevendo a tarefa
+        print("Lista de tarefas salva com sucesso! ")
+        break
+
+    
       
 
